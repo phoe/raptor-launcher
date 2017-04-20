3303e0f438386cd53b61ea306c138fd2d549e83b
Michal Herda <michal.herda@ericsson.com>
2017-04-20 16:21:59 +0200

    refactor, make backend-furcadia, refactor readme

--- README.md
+++ README.md
@@ -12,3 +12,18 @@ A variant launcher for Furcadia written in Common Lisp and (in the future) Commo
 Don't get too excited - it's just a mockup so far, but that's how I imagine the main screen of the launcher to be modeled.
 
 ![Mockup](mockup.png)
+
+## Current technique
+
+  1. Login into Furcadia CMS.
+    a. GET the login page.
+    b. Extract the authentication secret from HTML.
+    c. POST the login which includes the authentication secret.
+  2. Get the furc:// login string.
+    a. GET the FurEd page.
+    b. Extract the character login secret from HTML.
+    c. Fetch the character we want to login as.
+    d. POST the character with the character login secret.
+    e. Receive the login string from the HTTP response.
+  3. Launch Furcadia.
+    a. Launch the Furcadia client with the login string as cmdline argument.
--- backend-furcadia.lisp
+++ backend-furcadia.lisp
@@ -1,3 +1,30 @@
 ;;;; backend-furcadia.lisp
 
 (in-package :furcadia-launcher)
+
+(defun winepath (path)
+  "Calls the Wine utility WINEPATH on the provided unix path."
+  (trim-whitespace
+   (with-output-to-string (*standard-output*)
+     (uiop:run-program (list "winepath" "-w" path)
+                       :output t))))
+
+(defun generate-furcadia-command (furcadia-path login-link)
+  "Generates a Furcadia command suitable for use with UIOP:LAUNCH-PROGRAM,
+dependent on the provided Furcadia path and login link."
+  (let* ((exe-path (cat furcadia-path "Furcadia.exe"))
+         (path (if (uiop:os-windows-p)
+                   furcadia-path
+                   (winepath furcadia-path)))
+         (command (if (uiop:os-windows-p)
+                      (list exe-path)
+                      (list "wine" (winepath exe-path)))))
+    (append command
+            (list "-defaultpath" path
+                  "-followurl" login-link))))
+
+(defun launch-furcadia (furcadia-path login-link)
+  "Launches Furcadia, using the provided Furcadia path and login link."
+  (let ((command (generate-furcadia-command furcadia-path login-link)))
+    (uiop:chdir furcadia-path)
+    (uiop:launch-program command)))
