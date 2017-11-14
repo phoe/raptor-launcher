9cd9735d93ad772a439b6f1af09216e7fb4bee84
Michal Herda <michal.herda@ericsson.com>
2017-11-14 15:52:44 +0100

    add some docs and *LOADED-MODULES*

--- base/main-window.lisp
+++ base/main-window.lisp
@@ -6,7 +6,14 @@
 (in-package :raptor-launcher/base)
 (in-readtable :qtools)
 
-(define-widget main-window (QMainWindow) ())
+(defvar *loaded-modules* '()
+  "The list of all Raptor Launcher modules loaded into the Lisp image, by means
+of package designators.
+Each module is required to provide a compatible interface.")
+
+(define-widget main-window (QMainWindow) ()
+  (:documentation
+    "The main window widget for Raptor Launcher."))
 
 (define-widget (main-window central-widget) (q+:make-qwidget)
   (setf (q+:central-widget main-window) central-widget))
