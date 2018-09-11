fbfd1c4f9810c0df2ea41d59095a9756ef770c0f
Michal Herda <michal.herda@ericsson.com>
2018-09-11 10:47:04 +0200

    minor refactor

--- util/util.lisp
+++ util/util.lisp
@@ -15,16 +15,20 @@
      (qtools:with-slots-bound (,class ,class)
        ,@body)))
 
+(declaim (inline rgba-argb))
+
 (defun rgba-argb (vector)
-  (let ((result (make-array (length vector)
-                            :element-type (array-element-type vector))))
-    (loop for i from 0 below (length vector) by 4
-          for a = (aref vector (+ 0 i))
-          for b = (aref vector (+ 1 i))
-          for c = (aref vector (+ 2 i))
-          for d = (aref vector (+ 3 i))
-          do (setf (aref result (+ 0 i)) d
-                   (aref result (+ 1 i)) a
-                   (aref result (+ 2 i)) b
-                   (aref result (+ 3 i)) c)
-          finally (return result))))
+  (declare (optimize speed))
+  (declare (type (simple-array (unsigned-byte 8) (*)) vector))
+  (loop with result = (make-array (length vector)
+                                  :element-type '(unsigned-byte 8))
+        for i from 0 below (length vector) by 4
+        for a = (aref vector (+ 0 i))
+        for b = (aref vector (+ 1 i))
+        for c = (aref vector (+ 2 i))
+        for d = (aref vector (+ 3 i))
+        do (setf (aref result (+ 0 i)) d
+                 (aref result (+ 1 i)) a
+                 (aref result (+ 2 i)) b
+                 (aref result (+ 3 i)) c)
+        finally (return result)))
