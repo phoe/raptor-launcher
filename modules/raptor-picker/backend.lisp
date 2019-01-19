91c571f495d6969d52909c28e8830491691abb1c
Michał "phoe" Herda <phoe@disroot.org>
2019-01-19 01:54:40 +0100

    refactor signals and slots

--- modules/raptor-picker/backend.lisp
+++ modules/raptor-picker/backend.lisp
@@ -27,15 +27,21 @@
       (t (note t :warn *multiple-configs* (mapcar #'class-of configs))
        (accounts (first configs))))))
 
-;;; Sync
-
 (defmethod sync ((picker raptor-picker))
   (signal! picker (sync-started)))
 
-(define-signal (raptor-picker sync-started) ())
+(defmacro define-picker-step
+    ((picker signal-name) (&rest slot-arguments) &body body)
+  `(progn
+     (define-signal (,picker ,signal-name) ,(mapcar #'second slot-arguments))
+     (define-slot (,picker ,(symbolicate "GOT-" signal-name)) ,slot-arguments
+       (declare (connected ,picker
+                           (,signal-name ,@(mapcar #'second slot-arguments))))
+       ,@body)))
 
-(define-slot (raptor-picker got-sync-started) ()
-  (declare (connected raptor-picker (sync-started)))
+;;; Steps
+
+(define-picker-step (raptor-picker sync-started) ()
   (note t :info "Synchronization started.")
   (let ((loading-screen (loading-screen raptor-picker)))
     (q+:hide furre-list)
@@ -50,112 +56,68 @@
     (dolist (account accounts)
       (bag-insert (bag-of petri-net 'credentials) account))
     (setf (petri-net-of raptor-picker) petri-net)
-    (make-thread (curry #'execute-sync raptor-picker)
-                 :name "Raptor Picker Petri net thread")))
+    (make-thread
+     (named-lambda execute-sync ()
+       (let* ((petri-net (petri-net-of raptor-picker))
+              (errorp (nth-value 1 (funcall petri-net :ignore-errors t))))
+         (signal! raptor-picker (sync-complete bool) (not errorp))))
+     :name "Raptor Picker Petri net thread")))
 
-(defun execute-sync (picker)
-  (let* ((petri-net (petri-net-of picker))
-         (errorp (nth-value 1 (funcall petri-net :ignore-errors t))))
-    (if errorp
-        (note t :warn "Synchronization complete, but errors have occurred.")
-        (note t :info "Synchronization complete."))
-    (apply #'note t :info "Downloaded ~D accounts, ~D furres, ~D costumes, ~D ~
-portraits, ~D specitags, and ~D images."
-           (mapcar #'bag-count
-                   (mapcar (curry #'bag-of petri-net)
-                           '(accounts furres costumes
-                             portraits specitags images))))
-    (signal! picker (sync-complete bool) (not errorp))))
-
-;;; Login
-
-(define-signal (raptor-picker login-completed) (string)) ;; email
-
-(define-slot (raptor-picker got-login-completed) ((email string))
+(define-picker-step (raptor-picker login-completed) ((email string))
   (declare (connected raptor-picker (login-completed string)))
   (incf (current (loading-screen raptor-picker) 'progress-logins)))
 
-;;; Account downloaded
-
-(define-signal (raptor-picker account-downloaded)
-               (string int)) ;; email nfurres
-
-(define-slot (raptor-picker got-account-downloaded)
-             ((email string) (nfurres int))
+(define-picker-step (raptor-picker account-downloaded)
+    ((email string) (nfurres int))
   (declare (connected raptor-picker (account-downloaded string int)))
   (incf (current (loading-screen raptor-picker) 'progress-accounts))
   (incf (maximum (loading-screen raptor-picker) 'progress-furres) nfurres))
 
-;;; Furre downloaded
-
-(define-signal (raptor-picker furre-downloaded)
-               (string int int int)) ;; sname nspecitags nportraits ncostumes
-
-(define-slot (raptor-picker got-furre-downloaded)
-             ((sname string) (nspecitags int) (nportraits int) (ncostumes int))
+(define-picker-step (raptor-picker furre-downloaded)
+    ((sname string) (nspecitags int) (nportraits int) (ncostumes int))
   (declare (connected raptor-picker (furre-downloaded string int int int)))
   (incf (current (loading-screen raptor-picker) 'progress-furres))
   (incf (current (loading-screen raptor-picker) 'progress-costumes))
   (incf (maximum (loading-screen raptor-picker) 'progress-portraits) nportraits)
   (incf (maximum (loading-screen raptor-picker) 'progress-costumes) ncostumes))
 
-;;; Costume downloaded
-
-(define-signal (raptor-picker costume-downloaded) (string int)) ;; sname id
-
-(define-slot (raptor-picker got-costume-downloaded)
-             ((sname string) (ncostumes int))
+(define-picker-step (raptor-picker costume-downloaded)
+    ((sname string) (ncostumes int))
   (declare (connected raptor-picker (costume-downloaded string int)))
   (incf (current (loading-screen raptor-picker) 'progress-costumes)))
 
-;;; Specitag downloaded
-
-(define-signal (raptor-picker specitag-downloaded) (string int)) ;; sname id
-
-(define-slot (raptor-picker got-specitag-downloaded)
-             ((sname string) (nspecitags int))
+(define-picker-step (raptor-picker specitag-downloaded)
+    ((sname string) (nspecitags int))
   (declare (connected raptor-picker (specitag-downloaded string int)))
   (incf (current (loading-screen raptor-picker) 'progress-specitags)))
 
-;;; Portrait downloaded
-
-(define-signal (raptor-picker portrait-downloaded) (string int)) ;; sname id
-
-(define-slot (raptor-picker got-portrait-downloaded)
-             ((sname string) (nportraits int))
+(define-picker-step (raptor-picker portrait-downloaded)
+    ((sname string) (nportraits int))
   (declare (connected raptor-picker (portrait-downloaded string int)))
   (incf (current (loading-screen raptor-picker) 'progress-portraits)))
 
-;;; Image list downloaded
-
-(define-signal (raptor-picker image-list-downloaded)
-               (string int)) ;; sname nimages
-
-(define-slot (raptor-picker got-image-list-downloaded)
-             ((sname string) (nimages int))
+(define-picker-step (raptor-picker image-list-downloaded)
+    ((sname string) (nimages int))
   (declare (connected raptor-picker (image-list-downloaded string int)))
   (incf (maximum (loading-screen raptor-picker) 'progress-images) nimages))
 
-;;; Image downloaded
-
-(define-signal (raptor-picker image-downloaded) (string int)) ;; sname id
-
-(define-slot (raptor-picker got-image-downloaded)
-             ((sname string) (nimages int))
+(define-picker-step (raptor-picker image-downloaded)
+    ((sname string) (nimages int))
   (declare (connected raptor-picker (image-downloaded string int)))
   (incf (current (loading-screen raptor-picker) 'progress-images)))
 
-;;; Join all threads and complete synchronization
-
-(define-signal (raptor-picker sync-complete) (bool)) ;; successp
-
-(define-slot (raptor-picker got-sync-complete) ((successp bool))
+(define-picker-step (raptor-picker sync-complete) ((successp bool))
   (declare (connected raptor-picker (sync-complete bool)))
-  (case successp
-    ((nil)
-     ;; TODO
-     )
-    ((t)
-     (q+:hide (loading-screen raptor-picker))
-     (q+:show furre-list)
-     (q+:show image))))
+  (let ((petri-net (petri-net-of raptor-picker)))
+    (if successp
+        (note t :info "Synchronization complete.")
+        (note t :warn "Synchronization complete, but errors have occurred."))
+    (apply #'note t :info "Downloaded ~D accounts, ~D furres, ~D costumes, ~D ~
+portraits, ~D specitags, and ~D images."
+           (mapcar #'bag-count
+                   (mapcar (curry #'bag-of petri-net)
+                           '(accounts furres costumes
+                             portraits specitags images)))))
+  (q+:hide (loading-screen raptor-picker))
+  (q+:show furre-list)
+  (q+:show image))
